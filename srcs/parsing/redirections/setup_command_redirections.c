524656cb74b2048b40282bb845da1785da58c317
FluffyPrincesseKoala <cyrillemaire1@gmail.com>
2021-03-08 13:34:17 +0100

    fix segfault

--- srcs/parsing/redirections/setup_command_redirections.c
+++ srcs/parsing/redirections/setup_command_redirections.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   setup_command_redirections.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cylemair <cylemair@student.42.fr>          +#+  +:+       +#+        */
+/*   By: koala <koala@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 18:36:21 by cylemair          #+#    #+#             */
-/*   Updated: 2021/03/06 11:30:00 by cylemair         ###   ########.fr       */
+/*   Updated: 2021/03/08 12:38:43 by koala            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,8 @@ int	setup_command_redirections(t_bash *data, t_vect *command)
 			redirection = redirection->next;
 		if (data->error)
 			return (FAIL);
-		redirection = redirection->next;
+		if (redirection)
+			redirection = redirection->next;
 	}
 	return (SUCCESS);
 }
--- srcs/parsing/utils/free_array.c
+++ srcs/parsing/utils/free_array.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_array.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cylemair <cylemair@student.42.fr>          +#+  +:+       +#+        */
+/*   By: koala <koala@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 17:47:33 by cylemair          #+#    #+#             */
-/*   Updated: 2021/03/06 11:30:00 by cylemair         ###   ########.fr       */
+/*   Updated: 2021/03/08 13:32:59 by koala            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void	free_array(char **array)
 	while (array && array[i])
 	{
 		ft_strdel(&array[i]);
-		array[i] = NULL;
 		i++;
 	}
 	if (array)
