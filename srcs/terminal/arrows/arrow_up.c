1dd3baa074e2a95fcf8ec80159f644e105cca053
FluffyPrincesseKoala <cyrillemaire1@gmail.com>
2021-03-16 13:26:36 +0100

    ok c bon

--- srcs/terminal/arrows/arrow_up.c
+++ srcs/terminal/arrows/arrow_up.c
@@ -6,16 +6,29 @@
 /*   By: cylemair <cylemair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 16:38:43 by cylemair          #+#    #+#             */
-/*   Updated: 2021/03/15 20:29:17 by cylemair         ###   ########.fr       */
+/*   Updated: 2021/03/16 13:26:21 by cylemair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vingt_et_un_sh.h"
 
-void	arrow_up(t_bash *data)
+static t_vect	*go_back(t_bash *data)
 {
+	t_vect	*cmd;
 	int		count;
 
+	cmd = data->vector;
+	count = data->history_stack;
+	if (!cmd->down)
+		while (count-- && cmd->up)
+			cmd = cmd->up;
+	else
+		cmd = cmd->up;
+	return (cmd);
+}
+
+void			arrow_up(t_bash *data)
+{
 	if (data->vector->up)
 	{
 		data->history_stack++;
@@ -23,17 +36,13 @@ void	arrow_up(t_bash *data)
 		term_put(SAVE);
 		clear_term(data->vector->line);
 		term_put(RESET_C);
-		count = data->history_stack;
-		if (!data->vector->down)
-			while (count-- && data->vector->up)
-				data->vector = data->vector->up;
-		else
-			data->vector = data->vector->up;
+		go_back(data);
 		print_rest(data->vector->line, data->iterator, NULL);
-		set_cursors(data, &data->cursor, data->vector->line, get_win_max_col());
+		set_cursors(data, &data->cursor,
+			data->vector->line, get_win_max_col());
 		if ((data->cursor = find_cursor_node(&data->cursor,
-			ft_strlen(data->vector->line), get_win_max_col(), data->prompt_len)))
-		{	
+		ft_strlen(data->vector->line), get_win_max_col(), data->prompt_len)))
+		{
 			data->x = ft_strlen(data->cursor->line);
 			data->y = get_y_cursor(data->cursor);
 		}
