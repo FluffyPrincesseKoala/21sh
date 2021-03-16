6a4addf254988f2cb2514cad58c08d837ba2a6f2
FluffyPrincesseKoala <cyrillemaire1@gmail.com>
2021-03-16 13:12:22 +0100

    ok c bon

--- srcs/terminal/utils/move_right.c
+++ srcs/terminal/utils/move_right.c
@@ -6,7 +6,7 @@
 /*   By: cylemair <cylemair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 14:57:29 by cylemair          #+#    #+#             */
-/*   Updated: 2021/03/06 11:42:39 by cylemair         ###   ########.fr       */
+/*   Updated: 2021/03/16 13:05:35 by cylemair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 void				move_right(t_bash *data)
 {
-	if (data->x + 1 == data->cursor->x_max || data->x == data->cursor->line_len
-	|| data->cursor->line[data->x] == '\n')
+	if (data->cursor && data->cursor->line
+	&& (data->x + 1 == data->cursor->x_max
+	|| data->x == data->cursor->line_len
+	|| (data->x > ft_strlen(data->cursor->line)
+	|| data->cursor->line[data->x] == '\n')))
 	{
 		if (data->cursor->next)
 			data->cursor = data->cursor->next;
